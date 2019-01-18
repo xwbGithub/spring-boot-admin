303bf18625f38651bc2ce616bfb889d59b77a638
a-xiewenbin <1056481167@qq.com>
2019-01-18 11:30:40 +0800

    file true commit

--- admin-client/src/main/java/com/springboot/admin/AdminClientApplication.java
+++ admin-client/src/main/java/com/springboot/admin/AdminClientApplication.java
@@ -1,4 +1,4 @@
-package com.springboot.admin.adminclient;
+package com.springboot.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- admin-server/src/main/java/com/springboot.admin/AdminServerApplication.java
+++ admin-server/src/main/java/com/springboot.admin/AdminServerApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAdminServer
-public class AdminClientApplication {
+@EnableAdminServer //开启AdminServer的功能
+public class AdminServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdminClientApplication.class, args);
+        SpringApplication.run(AdminServerApplication.class, args);
     }
 
 }
